52de8a12ff85884496b48801613f60b627c3d616
sunhwa <sumz.ko@gmail.com>
2023-08-01 20:16:16 +0900

    -: timer 관련 constants

---
+++ src/constants/timer.ts
@@ -0,0 +1,62 @@
+export const MIN_TIMER_MIN = 1;
+export const MAX_TIMER_MIN = 99;
+export const MIN_LIMIT_MIN = 1;
+
+export const DEAFAULT_TOTAL_MINUTES = 10;
+export const DEFAULT_TURN_MINUTES = 1;
+
+export const TURN = {
+  player1: "player1",
+  player2: "player2",
+} as const;
+
+export const STATUS = {
+  init: "init",
+  running: "running",
+  pause: "pause",
+  stop: "stop",
+  end: "end",
+} as const;
+
+export const TIMER_STATUS = {
+  init: "init",
+  running: "running",
+  wait: "wait",
+  pause: "pause",
+  stop: "stop",
+  end: "end",
+  endTurn: "endTurn",
+} as const;
+
+export const BTN_DISABLE_BY_STATUS = {
+  init: {
+    start: false,
+    pause: true,
+    stop: true,
+    reset: true,
+  },
+  running: {
+    start: true,
+    pause: false,
+    stop: false,
+    reset: true,
+  },
+  pause: {
+    start: false,
+    pause: true,
+    stop: true,
+    reset: false,
+  },
+  stop: {
+    start: true,
+    pause: true,
+    stop: true,
+    reset: false,
+  },
+  end: {
+    start: true,
+    pause: true,
+    stop: true,
+    reset: false,
+  },
+} as const;

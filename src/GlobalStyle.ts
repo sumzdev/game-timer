fbefb718951d8eb0f865d9b7ae9d4bf153ce6bfb
sunhwa <sumz.ko@gmail.com>
2023-08-02 09:23:35 +0900

    Style: color 추가

--- src/GlobalStyle.ts
+++ src/GlobalStyle.ts
@@ -10,7 +10,16 @@ const GlobalStyle = css`
   }
 
   body {
+    --font-color: #444444;
     --btn-color: #1976d2;
+    --green: #c6ecc6;
+    --red: #ecb9b9;
+    --yellow: #fff8be;
+    --gray: #cbcaca;
+    --border-green: #70bc70;
+    --border-gray: #9e9d9d;
+    --border-red: #c96861;
+    --border-yellow: #e8d959;
   }
 
   html,
